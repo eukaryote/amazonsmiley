ce9c0f7c5cf59e18a4c1f49b2ae38886d804b320
eukaryote <sapientdust+github@gmail.com>
2017-11-18 14:25:01 -0800

    remove unnecessary code that was testing requestId
     - was preventing smile redirect from happening when
       https everywhere had already redirected non-https
       amazon to https amazon, and doesn't seem to be necessary
       to avoid looping, which was the original reason for storing
       the request id and comparing it

--- background.js
+++ background.js
@@ -1,12 +1,7 @@
 const TARGET_URL_REGEX = new RegExp("^https?://(www\.)?amazon\.(com|co\.uk|de)/?(?!ap/signin$)");
-let lastRequestId = null;
 
 function beforeRequest(requestDetails) {
-  const check = lastRequestId !== requestDetails.requestId
-                  && requestDetails.type === 'main_frame'
-                  && requestDetails.method === 'GET';
-  lastRequestId = requestDetails.requestId;
-  if (check) {
+  if (requestDetails.type === 'main_frame' && requestDetails.method === 'GET') {
     const newUrl = requestDetails.url.replace(TARGET_URL_REGEX, "https://smile.amazon.$2/");
     if (newUrl !== requestDetails.url) {
       return {redirectUrl: newUrl};
